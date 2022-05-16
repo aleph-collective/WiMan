5d3f63e52605e8be9413819b14ba198f35c2cf94
Morar Andrei <whiteex@icloud.com>
2022-05-16 15:03:58 +0300

    Starting to port the UE4 update to UE5.

--- Source/WiMan/Private/WinMan.cpp
+++ Source/WiMan/Private/WinMan.cpp
@@ -2,4 +2,130 @@
 
 
 #include "WinMan.h"
+#include "TimerManager.h"
+#include "Blueprint/UserWidget.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/CanvasPanelSlot.h"
 
+void UWinMan::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if(RescaleCornerButtonRef)
+	{
+		if(!RescaleCornerButtonRef->OnPressed.IsBound())
+		{
+			RescaleCornerButtonRef->OnPressed.AddDynamic(this, &UWinMan::RescaleOnPressed);
+		}
+
+		if(!RescaleCornerButtonRef->OnReleased.IsBound())
+		{
+			RescaleCornerButtonRef->OnReleased.AddDynamic(this, &UWinMan::StopUpdateScaleLocation);
+		}
+	}
+
+	if(TitleButtonRef)
+	{
+		if(!TitleButtonRef->OnPressed.IsBound())
+		{
+			TitleButtonRef->OnPressed.AddDynamic(this, &UWinMan::DragOnPressed);
+		}
+
+		if(!TitleButtonRef->OnReleased.IsBound())
+		{
+			TitleButtonRef->OnReleased.AddDynamic(this, &UWinMan::StopUpdateDragLocation);
+		}
+	}
+}
+
+void UWinMan::Drag()
+{
+	CanvasAsSlot = Cast<UCanvasPanelSlot>(CanvasRef->Slot);
+	MousePos = UWidgetLayoutLibrary::GetMousePositionOnViewport(GetWorld());
+	CanvasAsSlot->SetPosition(MousePos - MousePosOffset);
+}
+
+void UWinMan::Rescale()
+{
+	CanvasAsSlot = Cast<UCanvasPanelSlot>(CanvasRef->Slot);
+	MousePos = UWidgetLayoutLibrary::GetMousePositionOnViewport(GetWorld());
+	CanvasAsSlot->SetSize((MousePos - MousePosOffset) - (CanvasAsSlot->GetPosition() + CanvasAsSlot->GetSize()) + CanvasAsSlot->GetSize());
+}
+
+void UWinMan::SetDragClickLocDiff()
+{
+	CanvasAsSlot = Cast<UCanvasPanelSlot>(CanvasRef->Slot);
+	FVector2D PosOnViewport = UWidgetLayoutLibrary::GetMousePositionOnViewport(GetWorld());
+	MousePosOffset = PosOnViewport - CanvasAsSlot->GetPosition();
+}
+
+void UWinMan::SetRescaleClickLocDiff()
+{
+	CanvasAsSlot = Cast<UCanvasPanelSlot>(CanvasRef->Slot);
+	FVector2D PosOnViewport = UWidgetLayoutLibrary::GetMousePositionOnViewport(GetWorld());
+	MousePosOffset = PosOnViewport - (CanvasAsSlot->GetPosition() + CanvasAsSlot->GetSize());
+}
+
+bool UWinMan::CheckMinRes()
+{
+	CanvasAsSlot = Cast<UCanvasPanelSlot>(CanvasRef->Slot);
+	return CanvasAsSlot->GetSize().X < MinSize.X && CanvasAsSlot->GetSize().Y < MinSize.Y;
+}
+
+void UWinMan::FixWindowRes()
+{
+	CanvasAsSlot = Cast<UCanvasPanelSlot>(CanvasRef->Slot);
+	CanvasAsSlot->SetSize(MinSize);
+}
+
+void UWinMan::UpdateDragLocation()
+{
+	if (bCanDrag) {
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UWinMan::UpdateDragLocation, 0.02f, true);
+		Drag();
+	}
+}
+
+void UWinMan::StopUpdateDragLocation()
+{
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+}
+
+void UWinMan::UpdateScaleLocation()
+{
+	if (bCanRescale) {
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UWinMan::UpdateScaleLocation, 0.02f, true);
+		if (CheckMinRes()) {
+			StopUpdateScaleLocation();
+			FixWindowRes();
+		} else {
+			Rescale();
+		};
+	};
+}
+
+void UWinMan::StopUpdateScaleLocation()
+{
+	CanvasAsSlot = Cast<UCanvasPanelSlot>(RescaleCornerButtonRef->Slot);
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+	CanvasAsSlot->SetPosition(FVector2D(0, 0));
+}
+
+void UWinMan::DragOnPressed()
+{
+	SetDragClickLocDiff();
+	UpdateDragLocation();
+}
+
+void UWinMan::RescaleOnPressed()
+{
+	SetRescaleClickLocDiff();
+	UpdateScaleLocation();
+}
+
+void UWinMan::ConfigReferences(UCanvasPanel* Canvas, UButton* TitleButton, UButton* CornerButton)
+{
+	CanvasRef = Canvas;
+	TitleButtonRef = TitleButton;
+	RescaleCornerButtonRef = CornerButton;
+}
--- Source/WiMan/Public/WinMan.h
+++ Source/WiMan/Public/WinMan.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/CanvasPanel.h"
+#include "Components/Button.h"
 #include "WinMan.generated.h"
 
 /**
@@ -15,16 +17,62 @@ class WIMAN_API UWinMan : public UUserWidget
 	GENERATED_BODY()
 	
 public:
+	virtual void NativeConstruct() override;
+
+public:
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Data")
 		FVector2D MousePos;
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Data")
 		FVector2D MousePosOffset;
 
-	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Settings")
-		FLinearColor WindowColor = FLinearColor(0.02, 0.02, 0.02, 0.975);
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Data")
+		FVector2D MinSize = FVector2D(480, 360);
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings")
-		bool bIsDragEnable = true;
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings")
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "WiMan: Settings")
+		FLinearColor WindowColor = FLinearColor(0.02, 0.02, 0.02, 0.975);
+	
+	UCanvasPanel* CanvasRef;
+	UCanvasPanelSlot* CanvasAsSlot;
+	UButton* TitleButtonRef;
+	UButton* RescaleCornerButtonRef;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "WiMan: Settings")
+		bool bCanDrag = true;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "WiMan: Settings")
+		bool bCanRescale = true;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "WiMan: Settings")
 		bool bEnableDebug = false;
+	
+	FTimerHandle TimerHandle;
+
+	UFUNCTION(BlueprintCallable, Category = "WiMan")
+		void Drag();
+	UFUNCTION(BlueprintCallable, Category = "WiMan")
+		void Rescale();
+
+	UFUNCTION(BlueprintCallable, Category = "WiMan")
+		void SetDragClickLocDiff();
+	UFUNCTION(BlueprintCallable, Category = "WiMan")
+		void SetRescaleClickLocDiff();
+	UFUNCTION(BlueprintCallable, Category = "WiMan")
+		bool CheckMinRes();
+	UFUNCTION(BlueprintCallable, Category = "WiMan")
+		void FixWindowRes();
+
+	UFUNCTION(BlueprintCallable, Category = "WiMan: Update")
+		void UpdateDragLocation();
+	UFUNCTION(BlueprintCallable, Category = "WiMan: Update")
+		void StopUpdateDragLocation();
+	UFUNCTION(BlueprintCallable, Category = "WiMan: Update")
+		void UpdateScaleLocation();
+	UFUNCTION(BlueprintCallable, Category = "WiMan: Update")
+		void StopUpdateScaleLocation();
+
+	UFUNCTION(BlueprintCallable, Category = "WiMan: OnPressed")
+		void DragOnPressed();
+	UFUNCTION(BlueprintCallable, Category = "WiMan: OnPressed")
+		void RescaleOnPressed();
+
+	UFUNCTION(BlueprintCallable, Category = "WiMan: Settings")
+		void ConfigReferences(UCanvasPanel* Canvas, UButton* TitleButton, UButton* CornerButton);
 };
